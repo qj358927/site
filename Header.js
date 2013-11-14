d73be7cb744256da2f7ac5a3e3632aecafd2bc4b
qj358927 <qj358927@gmail.com>
2013-11-13 19:13:52 -0500

    Changed quotations to fit it in javascript

--- Header.js
+++ Header.js
@@ -1,40 +1,40 @@
 <script>
 
-  document.write(" <div id="container">
+  document.write(" <div id='container'>
         
-                <div id="logo">
-                <a href="frame.html">
-                <img src="acm.jpg">
+                <div id='logo'>
+                <a href='frame.html'>
+                <img src='acm.jpg'>
                 <a>
                 </div>
 
-                <div id="header">
+                <div id='header'>
                 <h1>Association of Computer Machinery</h1>
                 </div>
                 
-                <div id="links">
+                <div id='links'>
                 <br>
                 <b>Links</b>
                 <br>
                 <br>
-                <a href="index.html">Home</a>
+                <a href='index.html'>Home</a>
                 <br>
                 <br>
-                <a href="roster.html">Roster</a>
+                <a href='roster.html'>Roster</a>
                 <br>
                 <br>
-                <a href="calender.html">Event dates</a>
+                <a href='calender.html'>Event dates</a>
                 <br>
                 <br>
                 <br>
                 </div>
 
-                <div id="content">
+                <div id='content'>
                 <h1> This site is currently under construction.</h1>
-                <p> Check us out on Github!!! You can find us <a href="https://github.com/albanyacm/site">here</a>!</p>
+                <p> Check us out on Github!!! You can find us <a href='https://github.com/albanyacm/site'>here</a>!</p>
                 </div>
 
-                <div id="footer">
+                <div id='footer'>
                 Used html and css to make this. content by ACM.
                 </div>
         </div>");
