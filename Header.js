e4f4d003eff834d00ee257257df73b3be67ca240
qj358927 <qj358927@gmail.com>
2013-11-13 19:10:17 -0500

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,43 @@
+<script>
+
+  document.write(" <div id="container">
+        
+                <div id="logo">
+                <a href="frame.html">
+                <img src="acm.jpg">
+                <a>
+                </div>
+
+                <div id="header">
+                <h1>Association of Computer Machinery</h1>
+                </div>
+                
+                <div id="links">
+                <br>
+                <b>Links</b>
+                <br>
+                <br>
+                <a href="index.html">Home</a>
+                <br>
+                <br>
+                <a href="roster.html">Roster</a>
+                <br>
+                <br>
+                <a href="calender.html">Event dates</a>
+                <br>
+                <br>
+                <br>
+                </div>
+
+                <div id="content">
+                <h1> This site is currently under construction.</h1>
+                <p> Check us out on Github!!! You can find us <a href="https://github.com/albanyacm/site">here</a>!</p>
+                </div>
+
+                <div id="footer">
+                Used html and css to make this. content by ACM.
+                </div>
+        </div>");
+
+
+</script>
